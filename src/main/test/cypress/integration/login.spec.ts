01ddf62848d1c4f1e0a252b2b5d6e19cdae4e804
Rafael Silva <rafaelsilvamap@gmail.com>
2021-04-30 09:43:58 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,16 @@
 describe('Login', () => {
-  it('Shuld load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('Shuld load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('have.css', 'background-color', 'rgb(255, 0, 0)')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('have.css', 'background-color', 'rgb(255, 0, 0)')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
