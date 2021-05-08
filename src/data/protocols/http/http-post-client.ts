31d7d618303452420ff6c6e601803b8273d86c58
Rafael Silva <rafaelsilvamap@gmail.com>
2021-05-08 17:28:40 -0300

    refactor: rename generic body and response type of http post client

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,10 +1,10 @@
 import { HttpResponse } from '.'
 
-export type HttpPostParams<T> = {
+export type HttpPostParams<BodyType = any> = {
   url: string
-  body?: T
+  body?: BodyType
 }
 
-export interface HttpPostClient<T, R> {
-  post: (params: HttpPostParams<T>) => Promise<HttpResponse<R>>
+export interface HttpPostClient<BodyType = any, ResponseType = any> {
+  post: (params: HttpPostParams<BodyType>) => Promise<HttpResponse<ResponseType>>
 }
--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -8,7 +8,7 @@ export enum HttpStatusCode {
   serverError = 500
 }
 
-export type HttpResponse<T> = {
+export type HttpResponse<BodyType = any> = {
   statusCode: HttpStatusCode
-  body?: T
+  body?: BodyType
 }
