0f0c06c89b19d8b59bbd972b334ca3b78f4073ec
Rafael Silva <rafaelsilvamap@gmail.com>
2021-05-11 17:42:52 -0300

    feat: getCurrentAccountAdapter

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -6,6 +6,9 @@ export const setCurrentAccountAdapter = (account: AccountModel): void => {
   if (!account?.accessToken) {
     throw new UnexpectedError()
   }
-
   makeLocalStorageAdapter().set('account', account)
+}
+
+export const getCurrentAccountAdapter = (): AccountModel => {
+  return makeLocalStorageAdapter().get('account')
 }
