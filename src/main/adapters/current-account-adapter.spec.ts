84da32eadf9593ea3d868ebc36a3a5690ce7b011
Rafael Silva <rafaelsilvamap@gmail.com>
2021-05-17 22:54:41 -0300

    refactor: remove unused code

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,4 +1,3 @@
-import { UnexpectedError } from '@/domain/errors'
 import { mockAccountModel } from '@/domain/test'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 import { setCurrentAccountAdapter, getCurrentAccountAdapter } from './current-account-adapter'
@@ -10,12 +9,6 @@ describe('CurrentAccountAdapter', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
-  })
-
-  test('Should throw UnexpectedError', async () => {
-    expect(() => {
-      setCurrentAccountAdapter(undefined)
-    }).toThrow(new UnexpectedError())
   })
 
   test('Should call LocalStorageAdapter.get with correct values', async () => {
