27f9659514b26f99e18a72c06c9a43c52d325300
Rafael Silva <rafaelsilvamap@gmail.com>
2021-05-20 12:18:48 -0300

    test: ensure SurveyList presents correct username on header

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -5,11 +5,24 @@ import faker from 'faker'
 describe('SurveyList', () => {
   beforeEach(() => {
     Helper.setLocalStorageItem('account', { accessToken: faker.datatype.uuid(), name: faker.name.findName() })
-    cy.visit('')
   })
 
   it('Should present error on UnexpectedError', () => {
     Http.mockUnexpectedError()
+    cy.visit('')
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+  })
+
+  it('Should logout on AccessDeniedError', () => {
+    Http.mockAccessDeniedError()
+    cy.visit('')
+    Helper.testUrl('/login')
+  })
+
+  it('Should logout on AccessDeniedError', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    const { name } = Helper.getLocalStorageItem('account')
+    cy.getByTestId('username').should('contain.text', name)
   })
 })
--- src/main/test/cypress/support/survey-list-mocks.ts
+++ src/main/test/cypress/support/survey-list-mocks.ts
@@ -1,3 +1,4 @@
 import * as Http from '../support/http-mocks'
 
 export const mockUnexpectedError = (): void => Http.mockServerError(/surveys/, 'GET')
+export const mockAccessDeniedError = (): void => Http.mockForbiddenError(/surveys/, 'GET')
