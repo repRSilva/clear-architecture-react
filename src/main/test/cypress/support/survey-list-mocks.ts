d7546d99ab95894df71fefc7802672e9be1dbbfc
Rafael Silva <rafaelsilvamap@gmail.com>
2021-05-22 10:57:10 -0300

    refactor: organize integration tests with fixtures

--- src/main/test/cypress/support/survey-list-mocks.ts
+++
@@ -1,4 +0,0 @@
-import * as Http from '../support/http-mocks'
-
-export const mockUnexpectedError = (): void => Http.mockServerError(/surveys/, 'GET')
-export const mockAccessDeniedError = (): void => Http.mockForbiddenError(/surveys/, 'GET')
