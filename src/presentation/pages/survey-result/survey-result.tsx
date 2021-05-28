f416a60a49b935047b2d2fc60f26a58013b0ff3b
Rafael Silva <rafaelsilvamap@gmail.com>
2021-05-27 22:31:17 -0300

    feat: ensure SurveyResult presents error if SaveSurveyResult fails

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 const SurveyResult: React.FC<Props> = ({ loadSurveyResult, saveSurveyResult }: Props) => {
   const handleError = useErrorHandler((error: Error) => {
-    setState(old => ({ ...old, surveyResult: null, error: error.message }))
+    setState(old => ({ ...old, surveyResult: null, isLoading: false, error: error.message }))
   })
 
   const [state, setState] = useState({
@@ -24,10 +24,10 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult, saveSurveyResult }: P
 
   const reload = (): void => setState(old => ({ isLoading: false, surveyResult: null, error: '', reload: !old.reload }))
   const onAnswer = (answer: string): void => {
-    setState(old => ({ ...old, isLoading: true }))
+    setState(old => ({ ...old, surveyResult: null, isLoading: true }))
     saveSurveyResult.save({ answer })
       .then()
-      .catch()
+      .catch(handleError)
   }
 
   useEffect(() => {
